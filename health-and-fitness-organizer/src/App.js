24a6ae1c0067a7073b152c1bcf19ce469f2be61b
michael.greene2 <michael.greene2@ey.com>
2023-04-18 17:26:20 -0500

    first commit

---
+++ health-and-fitness-organizer/src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div>Health & Fitness</div>
+    </div>
+  );
+}
+
+export default App;
